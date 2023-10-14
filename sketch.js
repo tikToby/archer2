e4e1e89fb399d7b1956b99a0d9fd43cb21903e75
tikToby <143013773+tikToby@users.noreply.github.com>
2023-10-14 10:24:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,94 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase;
+var computer, computerBase;
+
+var arrow;
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  playerBase = new PlayerBase(100, random(450, height - 300), 180, 150);
+  player = new Player(85, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    140,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
+
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
+  
+  arrow = new PlayerArrow(playerArcher.body.position.x, playerArcher.body.position.y, 100, 10);
+  
+}
+
+function draw() {
+  background(180);
+
+  Engine.update(engine);
+
+  // Título
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("ARQUEIRO ÉPICO", width / 2, 100);
+
+ 
+  playerBase.display();
+  player.display();
+  
+
+  computerBase.display();
+  computer.display();
+  
+  playerArcher.display();
+  computerArcher.display()
+  
+    if(keyCode === 32){
+    arrow.display()
+    //arrow.shoot(playerArcher);
+
+   //  arrow.shoot(playerArcher.body.angle);
+
+
+     arrow.shoot(playerArcher.body.angle);
+  }
+}
+function showarrow(arrow,index){
+  arrow.display();
+  if(arrow.body.position.x>=width||arrow.body.position.y>=height-50){
+      Matter.World.remove(world,arrow.body);
+      arrow.splice(index,1)
+    }
+}
+
+
+
+
